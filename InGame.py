5df2fa2fe5cdf7751c113f506962a807e7515cba
Tiao <Tiao@whatever.com>
2015-12-09 00:16:51 -0500

    Added a heading for the program mainly meant for contributors Also took out API from functions as it is a global variable.

--- InGame.py
+++ InGame.py
@@ -1,6 +1,24 @@
 import requests
 API = "c916f23e-10bb-4371-bb5b-d5525baa733f"
 
+#################################################################
+#This is a program to find the game you are currently in        #
+#   it will find all the summoners, then get the ranked data    #
+#   of each summoner, their win rate on champ they are on.      #
+#   It will also give the runes and masteries the user is       #
+#`  currently using. More features will be added but this is    #
+#   a start.                                                    #
+#   Goal to have a graphical interface as well.                 #
+#                                                               #                              
+#   Program started by Dylan Guldy                              #
+#   Contributors:                                               #
+#       Jon Simmons                                             #
+#       Avaham Blaut                                            #
+#                                                               #
+#   Started December 7th 2015                                   #
+#                                                               #
+#   Thanks to RIOT API                                          #
+#################################################################
 #################################
 #Function getChampIdFromName    #
 #   takes in string champ name  #
@@ -37,7 +55,7 @@ def getChampNameFromId(Identity):
 #   Retuns the json data of the #
 #   rquested summoner           #
 #################################
-def requestSummonerData(region, summonerName, APIKey):
+def requestSummonerData(region, summonerName):
     URL = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + API
 #   print URL
 #   print "\n"
@@ -50,7 +68,7 @@ def requestSummonerData(region, summonerName, APIKey):
 #   ID integer                  #
 #   returns the ranked data json#
 #################################
-def requestRankedData(region, ID, APIKey):
+def requestRankedData(region, ID):
     URL = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v2.5/league/by-summoner/" + ID + "/entry?api_key=" + API
 #   print "\n"
 #   print URL
